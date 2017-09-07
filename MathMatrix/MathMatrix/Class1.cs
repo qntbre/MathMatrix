af695c72d16cff20598ecfd41a17bfd842910102
Electraxx <quentin.brechbuhl@gmail.com>
2017-09-07 14:19:47 +0200

    initial commit

---
+++ MathMatrix/MathMatrix/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace MathMatrix
+{
+    public class Calculation
+    {
+
+        #region yaw/pitch/roll
+
+        #region yaw
+
+        private void Yaw(double angle)
+        {
+            Matrix3D m = new Matrix3D();
+        }
+
+        #endregion
+
+        #region pitch
+
+        private void Pitch(double angle) { }
+
+        #endregion
+
+        #region roll
+
+        private void Roll(double angle) { }
+
+        #endregion        
+
+        #endregion
+
+
+    }
+}
