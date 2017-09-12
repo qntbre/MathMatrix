791e44427bfab88f17ec70d25795df7c8a53b16d
Electraxx <quentin.brechbuhl@gmail.com>
2017-09-12 07:59:58 +0200

    Fini ?

---
+++ MathMatrix/MathMatrix/Calculation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace MathMatrix
+{
+    public class Calculation
+    {
+
+        private static Quaternion EulerToQuaternion(double yaw, double pitch, double roll)
+        {
+            double cy = Math.Cos(yaw * 0.5);
+            double sy = Math.Sin(yaw * 0.5);
+            double cr = Math.Cos(roll * 0.5);
+            double sr = Math.Sin(roll * 0.5);
+            double cp = Math.Cos(pitch * 0.5);
+            double sp = Math.Sin(pitch * 0.5);
+
+            double w = cy * cr * cp + sy * sr * sp;
+            double x = cy * sr * cp - sy * cr * sp;
+            double y = cy * cr * sp + sy * sr * cp;
+            double z = sy * cr * cp - cy * sr * sp;
+
+            return new Quaternion(x, y, z, w);
+        }
+
+        public static Matrix3D GetModificationMatrix(double yaw, double pitch, double roll, Point3D rotationPoint, Vector3D offset)
+        {
+            //On obtient la matrice identitaire (celle de base)
+            Matrix3D modificationMatrix = Matrix3D.Identity;
+            //On converti les degrés Euler en Quaternions
+            Quaternion rotation = EulerToQuaternion(yaw, pitch, roll);
+
+            //Si il y a une translation à faire, on la fait
+            modificationMatrix.Translate(offset);
+            //On applique la rotation
+            modificationMatrix.RotateAt(rotation, rotationPoint);
+
+            return modificationMatrix;
+        }
+
+
+    }
+}
